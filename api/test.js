154ab5c4910cdd7064ad2119df67d97bfdaf0d4c
b2blosangeles <b2blosangeles@gmail.com>
2018-09-07 14:33:49 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,13 @@
-res.send(env);
+let cfg0 = {};
+var connection = pkg.mysql.createConnection(cfg0);
+connection.connect();
+var str = "SHOW TABLES; ";
+
+connection.query(str, function (error, results, fields) {
+  connection.end();
+  if (error) {
+    res.send({status:'error', value:error.message});
+  } else {
+    res.send({status:'success', value:results});
+  }
+});
